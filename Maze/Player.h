441632b370eb54b99b490bcd990483d31a661d63
stopresent <mamy4938@gmai.com>
2022-11-15 19:16:23 +0900

    Revert "오른손 법칙 END"
    This reverts commit 98839c414588fd91734bd6ce3839940fd32f15be.

--- Maze/Player.h
+++ Maze/Player.h
@@ -4,26 +4,16 @@ class Board;
 
 class Player
 {
-	enum
-	{
-		MOVE_TICK = 100,
-	};
 public:
 	void Init(Board* board);
 	void Update(uint64 deltaTick);
-
-	bool CanGo(Pos pos);
 
 	void SetPos(Pos pos) { _pos = pos; }
 	Pos GetPos() { return _pos; }
 
 private:
-	Pos				_pos = {};
-	int32			_dir = DIR_UP;
-	Board*			_board;
-
-	vector<Pos>		_path;
-	uint32			_pathIndex;
-	uint64			_sumTick;
+	Pos _pos = {};
+	int32 _dir = DIR_UP;
+	Board* _board;
 };
 
