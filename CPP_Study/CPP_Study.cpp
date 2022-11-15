e0fded7b57e164b364b850a4a6c424c2f352a994
stopresent <mamy4938@gmai.com>
2022-11-15 19:15:24 +0900

    Revert "Revert "연결 리스트 구현 연습""
    This reverts commit e7d3dff35df1d66834c9462f25f6fd428e74d4a4.

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -1,100 +1,213 @@
 #include <iostream>
 #include <vector>
+#include <list>
 using namespace std;
 
 template<typename T>
-class Vector
+class Node
 {
 public:
-	Vector()
+	Node() : _prev(nullptr), _next(nullptr), _data(T())
 	{
 
 	}
 
-	~Vector()
+	Node(const T& value) : _prev(nullptr), _next(nullptr), _data(value)
 	{
-		if (_data)
-			delete[] _data;
+
 	}
 
-	// [ ][ ][ ][ ][ ][ ] [ ]
+public:
+	Node* _prev;
+	Node* _next;
+	T		_data;
+};
+
+template<typename T>
+class Iterator
+{
+public:
+	Iterator() : _node(nullptr)
+	{
+
+	}
+
+	Iterator(Node<T>* node) : _node(node)
+	{
+
+	}
+
+	// ++it
+	Iterator& operator++()
+	{
+		_node = _node->_next;
+		return *this;
+	}
+
+	// it++
+	Iterator operator++(int)
+	{
+		Iterator<T> temp = *this;
+		_node = _node->_next;
+		return temp;
+	}
+
+	// --it
+	Iterator& operator--()
+	{
+		_node = _node->_prev;
+		return *this;
+	}
+
+	// it--
+	Iterator operator--(int)
+	{
+		Iterator<T> temp = *this;
+		_node = _node->_prev;
+		return temp;
+	}
+
+	// *it
+	T& operator*()
+	{
+		return _node->_data;
+	}
+
+	bool operator==(const Iterator& other)
+	{
+		return _node == other._node;
+	}
+
+	bool operator!=(const Iterator& other)
+	{
+		return _node != other._node;
+	}
+
+public:
+	Node<T>* _node;
+};
+
+template<typename T>
+class List
+{
+public:
+	List() : _size(0)
+	{
+		// [head] <-> ... <-> [tail]
+		_head = new Node<T>();
+		_tail = new Node<T>();
+		_head->_next = _tail;
+		_tail->_prev = _head;
+	}
+
+	~List()
+	{
+		while (_size > 0)
+			pop_back();
+
+		delete _head;
+		delete _tail;
+	}
+
 	void push_back(const T& value)
 	{
-		if (_size == _capacity)
-		{
-			// 증설 작업
-			int newCapacity = static_cast<int>(_capacity * 1.5);
-			if (newCapacity == _capacity)
-				newCapacity++;
-
-			reserve(newCapacity);
-		}
-
-		// 데이터 저장
-		_data[_size] = value;
-
-		// 데이터 개수 증가
-		_size++;
+		AddNode(_tail, value);
 	}
 
-	void reserve(int capacity)
+	void pop_back()
 	{
-		if (_capacity >= capacity)
-			return;
-
-		_capacity = capacity;
-
-		T* newData = new T[_capacity];
-
-		// 데이터 복사
-		for (int i = 0; i < _size; i++)
-			newData[i] = _data[i];
-
-		if (_data)
-			delete[] _data;
-
-		// 교체
-		_data = newData;
-	}
-
-	T& operator[](const int pos) { return _data[pos]; }
-
-	int size() { return _size; }
-	int capacity() { return _capacity; }
-
-	void clear()
-	{
-		if (_data)
-		{
-			delete[] _data;
-			_data = new T[_capacity];
-		}
-
-		_size = 0;
+		RemoveNode(_tail->_prev);
 	}
 
 private:
-	T* _data = nullptr;
-	int		_size = 0;
-	int		_capacity = 0;
+
+	// [head] <-> [1] <-> [prevNode] <-> [before] <-> [tail]
+	// [head] <-> [1] <-> [prevNode] <-> [newNode] <-> [before] <-> [tail]
+	Node<T>* AddNode(Node<T>* before, const T& value)
+	{
+		Node<T>* newNode = new Node<T>(value);
+		Node<T>* prevNode = before->_prev;
+
+		prevNode->_next = newNode;
+		newNode->_prev = prevNode;
+
+		newNode->_next = before;
+		before->_prev = newNode;
+
+		_size++;
+
+		return newNode;
+	}
+
+	// [head] <-> [prevNode] <-> [node] <-> [nextNode] <-> [tail]
+	// [head] <-> [prevNode] <-> [nextNode] <-> [tail]
+	Node<T>* RemoveNode(Node<T>* node)
+	{
+		Node<T>* prevNode = node->_prev;
+		Node<T>* nextNode = node->_next;
+
+		prevNode->_next = nextNode;
+		nextNode->_prev = prevNode;
+
+		delete node;
+
+		_size--;
+
+		return nextNode;
+	}
+
+	int size() { return _size; }
+
+public:
+	using iterator = Iterator<T>;
+
+	iterator begin() { return iterator(_head->_next); }
+	iterator end() { return iterator(_tail); }
+
+	// it '앞에' 추가
+	iterator insert(iterator it, const T& value)
+	{
+		Node<T>* node = AddNode(it._node, value);
+		return iterator(node);
+	}
+
+	iterator erase(iterator it)
+	{
+		Node<T>* node = RemoveNode(it._node);
+		return iterator(node);
+	}
+
+private:
+	Node<T>* _head;
+	Node<T>* _tail;
+	int			_size;
 };
 
 int main()
 {
-	vector<int> v;
+	List<int> li;
 
-	v.reserve(100);
-	cout << v.size() << " " << v.capacity() << endl;
-
-	for (int i = 0; i < 100; i++)
+	List<int>::iterator eraseIt;
+	// [ ] <-> [ ] <-> [ ] <-> [ ]  <-> [ ]  <-> [ ]
+	for (int i = 0; i < 10; i++)
 	{
-		v.push_back(i);
-		cout << v.size() << " " << v.capacity() << endl;
+		if (i == 5)
+		{
+			eraseIt = li.insert(li.end(), i);
+		}
+		else
+		{
+			li.push_back(i);
+		}
 	}
 
-	v.resize(10);
-	cout << v.size() << " " << v.capacity() << endl;
+	li.pop_back();
 
-	v.clear();
-	cout << v.size() << " " << v.capacity() << endl;
+	li.erase(eraseIt);
+
+	for (List<int>::iterator it = li.begin(); it != li.end(); it++)
+	{
+		cout << (*it) << endl;
+	}
+
 }
