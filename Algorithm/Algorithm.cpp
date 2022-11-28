796b7a007c972e187209f60c8cebc34ce270971c
stopresent <mamy4938@gmail.com>
2022-11-28 13:10:18 +0900

    Disjoint Set END

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -5,105 +5,146 @@
 #include <queue>
 using namespace std;
 
-// Q) map vs hash_map (C++11 표준 unordered_map)
+// 그래프/트리 응용
+// 최소 스패닝 트리 : (Minimum Spanning Tree)
 
-// map : Red-Black Tree
-// - 추가/탐색/삭제 O(logN)
+// 상호 배타적 집합 (Disjoint Set)
+// -> 유니온-파인드 Union-Find (합치기-찾기)
 
-// C# dictionary = C++ map (X)
-// C# dictionary = C++ unordered_map
-
-// hash_map (unordered_map)
-// - 추가/탐색/삭제 O(1)
-
-// 메모리를 내주고 속도를 취한다.
-
-void TestTable()
+void LineageBattleground()
 {
 	struct User
 	{
-		int userId = 0;
-		string userName;
+		int teamId;
+		// TODO
 	};
 
+	// TODO : UserManager
 	vector<User> users;
-	users.resize(1000);
-
-	users[777] = User{ 777,"stopresent" };
-
-	string name = users[777].userName;
-	cout << name << endl;
-	
-}
-
-void TestHash()
-{
-	struct User
+	for (int i = 0; i < 1000; i++)
 	{
-		int userId = 0;
-		string userName;
-	};
-
-	vector<User> users;
-	users.resize(1000);
-
-	const int userId = 123456789;
-	int key = (userId % 1000);
-
-	// 123456789번 유저 정보 세팅
-	users[key] = User{ userId, "stopresent" };
-
-	// 12345678번 유저 이름은?
-	User& user = users[key];
-	if (user.userId == userId)
-	{
-		string name = user.userName;
-		cout << name << endl;
+		users.push_back(User{ i });
 	}
 
-	// 충돌 문제
-	// 충돌이 발생한 자리를 대신해서 다른 빈자리를 찾아나서면 된다
-	// - 선형 조사법 (linear probing)
-	// hash(key)+1 -> hash(key)+2
-	// - 이차 조사법 (qudratic probing)
-	// hash(key)+1^2 -> hash(key)+2^2
-	// - etc
+	// 팀 동맹
+	// users[1] <-> users[5]
+	users[5].teamId = users[1].teamId;
 
+	// teamId=1 팀과 team=2 팀 통합
+	for (User& user : users)
+	{
+		if (user.teamId == 1)
+			user.teamId = 2;
+	}
+
+	// 찾기 연산 O(1)
+	// 합치기 연산 O(N)
 }
 
-void TestHashTableChaining()
+struct Node
 {
-	struct User
+	Node* leader;
+};
+
+// 시간 복잡도 : 트리의 높이에 비례한 시간이 걸림
+class NaiveDisjointSet
+{
+public:
+	NaiveDisjointSet(int n) : _parent(n)
 	{
-		int userId = 0;
-		string username;
-	};
-
-	vector<vector<User>> users;
-	users.resize(1000);
-
-	const int userId = 123456789;
-	int key = (userId % 1000);
-
-	// 123456789번 유저 정보 세팅
-	users[key].push_back(User{ userId, "stopresent" });
-
-	// 123456789번 유저 이름은?
-	vector<User>& bucket = users[key];
-	for (User& user : bucket)
-	{
-		if (user.userId == userId)
+		for (int i = 0; i < n; i++)
 		{
-			string name = user.username;
-			cout << name << endl;
+			_parent[i] = i;
 		}
 	}
 
-}
+	int Find(int u)
+	{
+		if (u == _parent[u])
+			return u;
+
+		return Find(_parent[u]);
+	}
+
+	// u와 v를 합친다 ( u가 v밑으로)
+	void Merge(int u, int v)
+	{
+		u = Find(u);
+		v = Find(v);
+
+		if (u == v)
+			return;
+		
+		_parent[u] = v;
+	}
+
+private:
+	vector<int> _parent;
+};
+
+// 트리가 한쪽으로 기우는 문제를 해결?
+// 트리를 합칠 때, 항상 [높이가 낮은 트리를] [높이가 높은 트리] 밑으로
+// (Union-By-Rank) 랭크에 의한 합치기 최적화
+
+// 시간 복잡도 O(Ackermann(n)) = O(1)
+class DisjointSet
+{
+public:
+	DisjointSet(int n) : _parent(n), _rank(n, 1)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			_parent[i] = i;
+		}
+	}
+
+	int Find(int u)
+	{
+		if (u == _parent[u])
+			return u;
+
+		return _parent[u] = Find(_parent[u]);
+	}
+
+	// u와 v를 합친다
+	void Merge(int u, int v)
+	{
+		u = Find(u);
+		v = Find(v);
+
+		if (u == v)
+			return;
+
+		if (_rank[u] > _rank[v])
+			swap(u, v);
+
+		// rank[u] <= rank[v] 보장됨
+		_parent[u] = v;
+
+		if (_rank[u] == _rank[v])
+			_rank[v]++;
+
+	}
+
+private:
+	vector<int> _parent;
+	vector<int> _rank;
+};
 
 int main()
 {
+	DisjointSet teams(1000);
 
-	TestHash();
+	teams.Merge(10, 1);
+	int teamId = teams.Find(1);
+	int teamId2 = teams.Find(10);
+
+	teams.Merge(3, 2);
+	int teamId3 = teams.Find(3);
+	int teamId4 = teams.Find(2);
+
+	teams.Merge(1, 3);
+	int teamId5 = teams.Find(1);
+	int teamId6 = teams.Find(3);
 
 }
