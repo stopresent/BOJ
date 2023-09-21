511930e57fba7275f948338d3bb0616785a25736
Stopresent <86364202+stopresent@users.noreply.github.com>
2023-09-21 17:55:13 +0900

    27323. 직사각형

---
+++ CodingTestStudy/Quest/27323. 직사각형/직사각형.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+
+	cin >> a >> b;
+
+	cout << a * b;
+}
