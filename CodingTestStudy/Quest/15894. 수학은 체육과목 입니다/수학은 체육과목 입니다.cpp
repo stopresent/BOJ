3480901f7749c96fce06e1f03f1c772eb452b992
stopresent <86364202+stopresent@users.noreply.github.com>
2023-09-24 02:00:11 +0900

    15894. 수학은 체육과목 입니다

---
+++ CodingTestStudy/Quest/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+long long n;
+
+int main()
+{
+	cin >> n;
+
+	cout << 4 * n;
+}
