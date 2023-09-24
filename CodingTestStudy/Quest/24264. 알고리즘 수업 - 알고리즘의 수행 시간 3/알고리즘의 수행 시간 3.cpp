834bab9a8fda0fa1e2755d9df4f16e250e218806
stopresent <86364202+stopresent@users.noreply.github.com>
2023-09-24 23:45:28 +0900

    24264. 알고리즘의 수행 시간 3

---
+++ CodingTestStudy/Quest/24264. 알고리즘 수업 - 알고리즘의 수행 시간 3/알고리즘의 수행 시간 3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+//MenOfPassion(A[], n) {
+//    sum < -0;
+//    for i < -1 to n
+//        for j < -1 to n
+//            sum < -sum + A[i] ¡¿ A[j]; # ÄÚµå1
+//            return sum;
+//}
+
+long long n;
+
+int main() 
+{
+	cin >> n;
+
+	cout << n * n << endl;
+	cout << 2;
+}
