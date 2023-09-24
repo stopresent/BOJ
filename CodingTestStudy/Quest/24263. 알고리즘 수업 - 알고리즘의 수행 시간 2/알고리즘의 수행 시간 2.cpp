8a16b0c43cee35325a715268029692c7dbfe8f80
stopresent <86364202+stopresent@users.noreply.github.com>
2023-09-24 22:18:09 +0900

    24263. 알고리즘의 수행 시간 2

---
+++ CodingTestStudy/Quest/24263. 알고리즘 수업 - 알고리즘의 수행 시간 2/알고리즘의 수행 시간 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+	cin >> n;
+
+	cout << n << endl;
+	cout << 1;
+}
