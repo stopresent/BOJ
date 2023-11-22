84e8fd9a0d3c2ec41bad68bec2e5c92bbfdb4846
stopresent <86364202+stopresent@users.noreply.github.com>
2023-11-23 04:39:32 +0900

    [FIX] 파일 자동 읽기 만들기

--- CodingTestStudy/CodingTestStudy.cpp
+++ CodingTestStudy/CodingTestStudy.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -13,5 +15,6 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+	freopen("input.txt", "rt", stdin);
 
 }
