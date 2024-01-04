f11e8f5c79a2037f50c4f47b089cc50fd53be42f
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-01-04 22:48:42 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int gcd(int a, int b) { //최대 공약수
+	int r = a % b;
+
+	while (r != 0) {
+		a = b;
+		b = r;
+		r = a % b;
+	}
+	return b;
+}
+
+int lcm(int a, int b) { //최소 공배수
+	return (a * b) / gcd(a, b);
+}
+
+int main(void) {
+
+	int a, b;
+
+	cin >> a >> b;
+
+	cout << gcd(a, b) << endl;
+	cout << lcm(a, b) << endl;
+
+	return 0;
+}
