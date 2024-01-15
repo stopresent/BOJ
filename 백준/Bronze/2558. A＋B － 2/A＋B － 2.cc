74d113687ce69075c2c7bbc9d48eddd5c2bfef5b
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-01-15 21:53:56 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen("input.txt", "rt", stdin);
+
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
+
+	return 0;
+}
