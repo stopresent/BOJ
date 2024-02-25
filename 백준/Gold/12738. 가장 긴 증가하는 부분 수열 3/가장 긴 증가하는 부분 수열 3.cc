3c8db5c605f4902bc0f51144619cf2cdb947cafb
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-02-25 17:51:39 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 188 ms, Memory: 12200 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int arr[1000001];
+vector<int> seq;
+int N, weight = 1000000000;
+
+void bs(int num)
+{
+	int low = 0, high = seq.size() - 1, mid;
+	int ret = 100000007; // 왜 이런 숫자로 잡는거지?
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		int here = seq[mid];
+		if (here >= num)
+		{
+			if (ret > mid)
+				ret = mid;
+			high = mid - 1;
+		}
+		else
+			low = mid + 1;
+	}
+
+	seq[ret] = num;
+}
+
+void findLis()
+{
+	seq.push_back(arr[0]);
+	for (int i = 1; i < N; ++i)
+	{
+		if (seq.back() < arr[i])
+			seq.push_back(arr[i]);
+		else
+			bs(arr[i]);
+	}
+}
+
+void solve()
+{
+	cin >> N;
+	int temp;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> temp;
+
+		arr[i] = temp + weight;
+	}
+
+	findLis();
+
+	cout << seq.size();
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen("input.txt", "rt", stdin);
+
+	solve();
+	
+	return 0;
+}
