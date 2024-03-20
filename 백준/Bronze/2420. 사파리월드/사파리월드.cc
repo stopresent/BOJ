757449292cd4c039ab23a0833af3579cfb274d19
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-03-20 22:57:30 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+void solve()
+{
+	long long a, b;
+	cin >> a >> b;
+	cout << abs(a - b);
+}
+
+int main() 
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
