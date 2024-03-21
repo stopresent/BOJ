ba06c5095b10f01a6e30737817a2b7f50e1f9ffd
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-03-21 23:53:41 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int n;
+void solve()
+{
+	//4 
+	cin >> n;
+	if (n % 2 == 0)
+		cout << "CY";
+	else
+		cout << "SK";
+}
+
+int main() 
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
