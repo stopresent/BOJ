7bab0f993c9160bc7661f93810ea60f4bb05760b
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-04-08 15:40:53 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	long long a, b;
+	cin >> a >> b;
+
+	long long tempA = a * a;
+	long long tempB = b * b;
+
+	cout << tempA - tempB;
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
