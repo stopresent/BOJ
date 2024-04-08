e1c9a7e61a1e3e2a06ef31f68ef1bbeec1396519
Stopresent <86364202+stopresent@users.noreply.github.com>
2024-04-08 15:37:44 +0900

    [ADD] 세팅 만들기

--- CodingTestStudy/CodingTestStudy.cpp
+++ CodingTestStudy/CodingTestStudy.cpp
@@ -1,20 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <iostream>
-#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
 #include <vector>
-#include <list>
-#include <stack>
-#include <queue>
-#include <map>
+#include <algorithm>
+#include <math.h>
+#include <climits>
 
 using namespace std;
 
+void solve()
+{
+
+}
+
 int main()
 {
+	FILE* stream;
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	freopen("input.txt", "rt", stdin);
+	freopen_s(&stream, "input.txt", "rt", stdin);
 
+	solve();
+
+	return 0;
 }
