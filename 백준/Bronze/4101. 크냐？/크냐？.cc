55ed6dd966e3a77ee805ea17844e7b6806e7081f
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-04-11 17:19:30 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int a = -1, b = -1;
+	while (true)
+	{
+		cin >> a >> b;
+
+		if (a == 0 && b == 0)
+			break;
+
+		if (a <= b)
+			cout << "No" << '\n';
+		else
+			cout << "Yes" << '\n';
+	}
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
