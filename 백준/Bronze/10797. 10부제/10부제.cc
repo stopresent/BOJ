edcbcf82c226c3a7d671ebc638337bd9855f77a8
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-04-21 18:07:51 +0900

    [Bronze IV] Title: 10부제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.cc
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int a;
+	cin >> a;
+
+	int ret = 0;
+	for (int i = 0; i < 5; ++i)
+	{
+		int temp;
+		cin >> temp;
+		if (a == temp)
+			ret++;
+	}
+
+	cout << ret;
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
