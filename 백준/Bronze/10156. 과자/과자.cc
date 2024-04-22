49028fc27c9cd83cd84e9eb306958c3c8274a511
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-04-22 18:47:55 +0900

    [Bronze IV] Title: 과자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.cc
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << max(0, a * b - c);
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
