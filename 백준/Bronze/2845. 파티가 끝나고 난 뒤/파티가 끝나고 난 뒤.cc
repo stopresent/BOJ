f1a8e15bae8d41f0e0ef15afe53e77b16ec4037d
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-04-23 18:49:18 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int a, b;
+	cin >> a >> b;
+	for (int i = 0; i < 5; ++i)
+	{
+		int temp;
+		cin >> temp;
+		cout << temp - (a * b) << " ";
+	}
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
