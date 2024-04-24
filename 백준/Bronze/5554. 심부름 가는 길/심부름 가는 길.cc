2a90f58594ff6e2772f0ce047ce811d8aa14c1aa
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-04-24 19:33:21 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cc
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int sum = 0;
+	for (int i = 0; i < 4; ++i)
+	{
+		int temp;
+		cin >> temp;
+		sum += temp;
+	}
+
+	cout << sum / 60 << endl;
+	cout << sum % 60;
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
