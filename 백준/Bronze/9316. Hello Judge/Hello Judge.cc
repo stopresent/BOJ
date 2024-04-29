5ed4c1377d4d66480fd1ab1300b6353d1d018110
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-04-29 13:56:07 +0900

    [Bronze IV] Title: Hello Judge, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		cout << "Hello World, Judge "<< i << "!" << endl;
+	}
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
