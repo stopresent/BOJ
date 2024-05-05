4b2c32284c4c715e0b73943ba494461ce54890eb
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-05-05 15:32:02 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int n, m, k;
+	cin >> n >> m >> k;
+	cout << k / m << " " << (k % m);
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
