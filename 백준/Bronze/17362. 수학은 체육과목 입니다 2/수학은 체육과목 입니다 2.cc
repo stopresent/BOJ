d894ece3e165346329f60d91fbe5768e4cdf3c1b
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-05-11 10:20:13 +0900

    [Bronze IV] Title: 수학은 체육과목 입니다 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.cc
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin >> n;
+	int temp = n % 8;
+	if (temp == 0)
+		cout << 2;
+	else if (temp == 6)
+		cout << 4;
+	else if (temp == 7)
+		cout << 3;
+	else
+		cout << temp;
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
