0f7484a93c73200e4a731a88303c10a8e8bfad33
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-05-12 10:03:42 +0900

    [Bronze IV] Title: 히스토그램, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		int temp;
+		cin >> temp;
+
+		for (int j = 0; j < temp; ++j)
+		{
+			cout << "=";
+		}
+		cout << '\n';
+	}
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
