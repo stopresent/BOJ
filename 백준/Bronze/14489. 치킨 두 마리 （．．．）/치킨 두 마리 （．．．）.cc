d06192859e9a2fa6127dae9954ad6ec58d9cdfd1
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-05-15 14:02:58 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.cc
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int a, b;
+	cin >> a >> b;
+	int c;
+	cin >> c;
+	if ((a + b) >= 2 * c)
+	{
+		cout << (a + b) - 2 * c;
+	}
+	else
+	{
+		cout << a + b;
+	}
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
