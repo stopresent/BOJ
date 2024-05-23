f908ddc47d4e16f5be3b2e27986bffabf63e4686
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-05-23 13:06:07 +0900

    [Bronze III] Title: 플러그, Time: 24 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Bronze/2010. 플러그/플러그.cc
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin >> n;
+	vector<int> v(n);
+
+	int ret = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> v[i];
+		ret += (v[i] - 1);
+	}
+
+	cout << ret + 1;
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
