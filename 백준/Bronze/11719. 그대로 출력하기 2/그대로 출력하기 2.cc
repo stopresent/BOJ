4e753e6ddc71203ea522909a9368af6a12de9657
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-05-25 13:23:25 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+#include <string>
+
+using namespace std;
+
+void solve()
+{
+	string str;
+	while (getline(cin, str)) 
+	{
+		cout << str << endl;
+	}
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
