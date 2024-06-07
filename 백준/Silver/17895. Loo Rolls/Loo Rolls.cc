61b382bb064768b1c695307253fb14660654e8de
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-06-07 21:33:11 +0900

    [Silver V] Title: Loo Rolls, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17895. Loo Rolls/Loo Rolls.cc
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	long long l, n;
+	cin >> l >> n;
+
+	int k = 1;
+
+	while (l % n != 0)
+	{
+		n = n - (l % n);
+		k++;
+	}
+
+	cout << k;
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
