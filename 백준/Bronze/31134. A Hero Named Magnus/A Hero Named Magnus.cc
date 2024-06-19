5e082ad3ac075a616568e05df204d6b2a55b635d
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-06-19 17:55:00 +0900

    [Bronze IV] Title: A Hero Named Magnus, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31134. A Hero Named Magnus/A Hero Named Magnus.cc
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; ++i) {
+        long long x;
+        cin >> x;
+        cout << 2 * x - 1 << '\n';
+    }
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
