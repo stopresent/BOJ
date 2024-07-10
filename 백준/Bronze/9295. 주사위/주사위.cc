a519a5d46493fc9617534f97e3aa5e5d3cf5aa2e
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-07-11 07:45:45 +0900

    [Bronze IV] Title: 주사위, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << "Case " << i + 1 << ": " << a + b << '\n';
+	}
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
