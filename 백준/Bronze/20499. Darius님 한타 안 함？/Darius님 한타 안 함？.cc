4e2561ada1065f73699d7aa49a5c1d4a32aea9b3
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-07-12 16:27:58 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int k, d, a;
+	scanf("%d/%d/%d", &k, &d, &a);
+	if (k + a < d || d == 0)
+		cout << "hasu";
+	else
+		cout << "gosu";
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
