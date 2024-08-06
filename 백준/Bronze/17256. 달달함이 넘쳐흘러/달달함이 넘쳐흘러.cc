9ce676d580de6ed84dced4ff7d291f0142eb7813
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-08-06 18:04:50 +0900

    [Bronze IV] Title: 달달함이 넘쳐흘러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int ax, ay, az, cx, cy, cz;
+	cin >> ax >> ay >> az >> cx >> cy >> cz;
+
+	cout << cx - az << " ";
+	cout << cy / ay << " ";
+	cout << cz - ax << " ";
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
