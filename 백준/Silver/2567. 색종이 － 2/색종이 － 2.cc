fa6716ca6ec11a98d2e0d161e62d51298b322472
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-08-07 17:47:44 +0900

    [Silver IV] Title: 색종이 - 2, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.cc
@@ -0,0 +1,112 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+int dy[] = { 0, 1, 0, -1, 1, 1, -1, -1 };
+int dx[] = { 1, 0, -1, 0, 1, -1, 1, -1 };
+const int side = 110;
+vector<vector<int>> board(101, vector<int>(101, 0));
+vector<vector<int>> newboard(101, vector<int>(101, 0));
+
+void PrintBoard()
+{
+	for (int y = 0; y < 100; ++y)
+	{
+		for (int x = 0; x < 100; ++x)
+		{
+			cout << board[y][x];
+		}
+
+		cout << endl;
+	}
+}
+
+void PrintNewBoard()
+{
+	int cnt = 0;
+	for (int y = 0; y < 100; ++y)
+	{
+		for (int x = 0; x < 100; ++x)
+		{
+			cout << newboard[y][x];
+			if (newboard[y][x] >= 1)
+			{
+				cnt++;
+
+			}
+		}
+
+		cout << endl;
+	}
+
+	cout << cnt;
+}
+
+void solve()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		int x, y;
+		cin >> x >> y;
+
+		for (int j = 0; j < 10; ++j)
+		{
+			for (int k = 0; k < 10; ++k)
+			{
+				board[y + j][x + k]++;
+			}
+		}
+	}
+	//PrintBoard();
+
+	int ret = 0;
+	for (int y = 0; y < 100; ++y)
+	{
+		for (int x = 0; x < 100; ++x)
+		{
+			if (board[y][x] == 0)
+				continue;
+
+			int cnt = 0;
+			for (int i = 0; i < 4; ++i)
+			{
+				int ny = y + dy[i];
+				int nx = x + dx[i];
+
+				if (board[ny][nx] == 0)
+				{
+					ret++;
+				}
+			}
+
+		}
+	}
+
+	//PrintNewBoard();
+	cout << ret;
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
