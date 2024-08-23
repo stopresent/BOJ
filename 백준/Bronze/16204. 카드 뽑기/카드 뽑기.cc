df941a963fbdffc654a9c6a2555e91765a1013b7
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-08-23 15:19:54 +0900

    [Bronze IV] Title: 카드 뽑기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16204. 카드 뽑기/카드 뽑기.cc
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int n, m, k;
+	cin >> n >> m >> k;
+
+	if (m >= k)
+	{
+		cout << k + (n - m);
+	}
+	else
+	{
+		cout << m + (n - k);
+	}
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
