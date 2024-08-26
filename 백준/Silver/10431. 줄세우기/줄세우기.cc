6e8fb4b8538f4f19ea54d9b344c14a2aa013cc87
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-08-26 17:05:55 +0900

    [Silver V] Title: 줄세우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10431. 줄세우기/줄세우기.cc
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void pintheight(vector<int> heightes)
+{
+	for (int i = 0; i < 20; ++i)
+	{
+		cout << heightes[i] << " ";
+	}
+	cout << endl;
+}
+
+void solve()
+{
+	int t;
+	cin >> t;
+	vector<int> testcases(t + 1);
+	vector<int> ans(t);
+	for (int i = 0; i < t; ++i)
+	{
+		cin >> testcases[i];
+
+		vector<int> heightes(20);
+		for (int j = 0; j < 20; ++j)
+		{
+			cin >> heightes[j];
+			int height = heightes[j];
+			for (int k = j - 1; k >= 0; --k)
+			{
+				//pintheight(heightes);
+				if (heightes[k] > height)
+				{
+					swap(heightes[k], heightes[k + 1]);
+					ans[i]++;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < t; ++i)
+	{
+		cout << testcases[i] << " ";
+
+		cout << ans[i] << '\n';
+	}
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
