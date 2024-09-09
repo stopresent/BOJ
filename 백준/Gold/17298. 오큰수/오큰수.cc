af59f04d5d6758e3779181c62924fb9231b38950
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-09-10 08:02:14 +0900

    [Gold IV] Title: 오큰수, Time: 196 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -7,54 +7,46 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <math.h>
+#include <climits>
 #include <stack>
 
 using namespace std;
 
 int n;
-vector<int> seq;
-vector<int> NGE;
+vector<int> v, ret;
 stack<int> s;
-void setting()
-{
-	cin >> n;
-	seq = vector<int>(n + 1);
-	NGE = vector<int>(n + 1);
-	for (int i = 1; i <= n; ++i)
-	{
-		cin >> seq[i];
-	}
-}
 
 void solve()
 {
-	for (int i = n; i >= 1; --i)
+	cin >> n;
+	v = vector<int>(1000005);
+	ret = vector<int>(1000005, -1);
+	for (int i = 0; i < n; ++i)
 	{
-		while (s.empty() == false && s.top() <= seq[i])
+		cin >> v[i];
+		while (s.size() && v[s.top()] < v[i])
+		{
+			ret[s.top()] = v[i];
 			s.pop();
-
-		if (s.empty())
-			NGE[i] = -1;
-		else
-			NGE[i] = s.top();
-
-		s.push(seq[i]);
+		}
+		s.push(i);
 	}
 
-	for (int i = 1; i <= n; ++i)
+	for (int i = 0; i < n; ++i)
 	{
-		cout << NGE[i] << " ";
+		cout << ret[i] << " ";
 	}
 }
 
 int main()
 {
+	FILE* stream;
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	//freopen("input.txt", "rt", stdin);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
 
-	setting();
 	solve();
 
 	return 0;
