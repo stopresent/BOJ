760013a550658dde96f66b86b5d0f54627ebabc6
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-09-14 13:18:57 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.cc
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+void solve()
+{
+	int a, b;
+	cin >> a >> b;
+	a /= 2;
+	cout << min(a, b);
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
