f38b37e99d5446ec42d61f6a866b5819d651c2dd
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-10-12 23:12:24 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE" << '\n';
+	cout << "N2 Bomber      Heavy Fighter  Limited    21        " << '\n';
+	cout << "J-Type 327     Light Combat   Unlimited  1         " << '\n';
+	cout << "NX Cruiser     Medium Fighter Limited    18        " << '\n';
+	cout << "N1 Starfighter Medium Fighter Unlimited  25        " << '\n';
+	cout << "Royal Cruiser  Light Combat   Limited    4         " << '\n';
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
