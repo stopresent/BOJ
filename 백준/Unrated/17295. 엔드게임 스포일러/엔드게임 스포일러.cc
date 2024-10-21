8e9332c20e66256c590d7bf49e4b2a15108d5689
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-10-21 18:14:53 +0900

    [Unrated] Title: 엔드게임 스포일러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/17295. 엔드게임 스포일러/엔드게임 스포일러.cc
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	cout << "Avengers: Endgame";
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
