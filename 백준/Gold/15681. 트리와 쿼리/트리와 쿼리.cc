0f89e4db5dd6ddef08919a9511649dd07b0d6d80
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-10-29 15:40:21 +0900

    [Gold V] Title: 트리와 쿼리, Time: 64 ms, Memory: 12880 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -5,24 +5,22 @@ using namespace std;
 int N, R, Q;
 vector<vector<int>> board;
 vector<int> visited;
-vector<int> parent;
-vector<int> dist;
-vector<int> sizes;
+vector<int> v;
 
 int dfs(int x)
 {
 	visited[x] = 1;
-	sizes[x] = 1;
+	v[x] = 1;
 
 	for (int i = 0; i < board[x].size(); ++i)
 	{
 		int next = board[x][i];
 		if (visited[next])
 			continue;
-		sizes[x] += dfs(next);
+		v[x] += dfs(next);
 	}
 
-	return sizes[x];
+	return v[x];
 }
 
 void solve()
@@ -30,9 +28,7 @@ void solve()
 	cin >> N >> R >> Q;
 	board = vector<vector<int>>(N + 1);
 	visited = vector<int>(N + 1);
-	parent = vector<int>(N + 1);
-	dist = vector<int>(N + 1);
-	sizes = vector<int>(N + 1);
+	v = vector<int>(N + 1);
 	for (int i = 0; i < N - 1; ++i)
 	{
 		int u, v;
@@ -47,59 +43,8 @@ void solve()
 	{
 		int u;
 		cin >> u;
-		cout << sizes[u] << '\n';
+		cout << v[u] << '\n';
 	}
-
-	/*parent[R] = R;
-	dist[R] = 0;
-	queue<int> q;
-	q.push(R);
-	visited[R] = 1;
-	while (q.empty() == false)
-	{
-		int here = q.front();
-		q.pop();
-
-		for (int i = 0; i < board[here].size(); ++i)
-		{
-			int next = board[here][i];
-			if (visited[next])
-				continue;
-			parent[next] = here;
-			dist[next] = dist[here] + 1;
-			visited[next] = 1;
-			q.push(next);
-		}
-	}
-
-	for (int i = 0; i < Q; ++i)
-	{
-		int u;
-		cin >> u;
-		visited = vector<int>(N + 1);
-		q.push(u);
-		int cnt = 1;
-		visited[u] = 1;
-		while (q.empty() == false)
-		{
-			int here = q.front();
-			q.pop();
-
-			for (int i = 0; i < board[here].size(); ++i)
-			{
-				int next = board[here][i];
-				if (next == parent[here])
-					continue;
-				if (visited[next])
-					continue;
-				visited[next] = 1;
-				q.push(next);
-				cnt++;
-			}
-		}
-
-		cout << cnt << '\n';
-	}*/
 }
 
 int main()
