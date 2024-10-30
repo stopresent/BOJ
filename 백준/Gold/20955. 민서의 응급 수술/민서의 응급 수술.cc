86ea73b56fa3d9a398165c4540abc03dd462b58f
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-10-30 10:01:49 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 100 ms, Memory: 2500 KB -BaekjoonHub

--- 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.cc
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.cc
@@ -1,57 +1,60 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
 
 using namespace std;
 
-vector<int> graph[100001];
-bool visited[100001];
-int n, m;
+vector<int> parent(100001);
 
-bool findCycle(int node, int parent) {
-    visited[node] = true;
-    bool hasCycle = false;
+// Find 함수: 경로 압축을 사용하여 부모 노드를 찾음
+int find(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = find(parent[x]);
+}
 
-    for (int next : graph[node]) {
-        if (!visited[next]) {
-            if (findCycle(next, node)) {
-                hasCycle = true;
-            }
-        } else if (next != parent) {
-            hasCycle = true;
-        }
+// Union 함수: 두 집합을 합침
+void unionSet(int a, int b) {
+    int rootA = find(a);
+    int rootB = find(b);
+    if (rootA != rootB) {
+        parent[rootB] = rootA;
     }
-
-    return hasCycle;
 }
 
 int main() {
+    int n, m;
     cin >> n >> m;
 
-    // 그래프 입력 받기
+    // 부모 배열 초기화
+    for (int i = 1; i <= n; i++) {
+        parent[i] = i;
+    }
+
+    int cycleCount = 0;
+
+    // 간선 입력 받기
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
+
+        if (find(u) == find(v)) {
+            // 두 노드가 이미 같은 집합에 속해있다면 사이클 발생
+            cycleCount++;
+        } else {
+            // 두 노드를 같은 집합으로 합침
+            unionSet(u, v);
+        }
     }
 
+    // 연결 요소의 개수 계산
     int components = 0;
-    int cycleCount = 0;
-
-    // 모든 노드에 대해 DFS 실행
     for (int i = 1; i <= n; i++) {
-        if (!visited[i]) {
-            // 연결 요소의 시작점에서 사이클이 있는지 탐색
-            if (findCycle(i, -1)) {
-                cycleCount++;
-            }
+        if (find(i) == i) {
             components++;
         }
     }
 
-    // 사이클을 제거하기 위해 필요한 간선 수 + 연결 요소를 하나로 만들기 위해 필요한 간선 수
-    int result = (m - (n - components)) + (components - 1);
+    // 결과 계산: 사이클의 개수 + (연결 요소의 개수 - 1)
+    int result = cycleCount + (components - 1);
     cout << result << '\n';
 
     return 0;
