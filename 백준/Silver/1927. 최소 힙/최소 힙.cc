2f53c1487dbca7efd2c9b1021b810b35e79db01a
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-11-01 19:41:25 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -1,84 +1,38 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <list>
-#include <stack>
-#include <queue>
-#include <map>
-
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
 using namespace std;
-
-int n;
-vector<int> heap;
-vector<int> ans;
-
-void push_heap(vector<int>& heap, int newValue)
-{
-	heap.push_back(newValue);
-	int idx = heap.size() - 1;
-
-	while (idx > 0 && heap[(idx - 1) / 2] > heap[idx])
-	{
-		swap(heap[idx], heap[(idx - 1) / 2]);
-		idx = (idx - 1) / 2;
-	}
-}
-
-void pop_heap(vector<int>& heap)
-{
-	if (heap.size() == 0)
-	{
-		ans.push_back(0);
-		return;
-	}
-	ans.push_back(heap[0]);
-	heap[0] = heap.back();
-	heap.pop_back();
-	int here = 0;
-	while (true)
-	{
-		int left = here * 2 + 1, right = here * 2 + 2;
-		if (left >= heap.size())
-			break;
-		int next = here;
-		if (heap[next] > heap[left])
-			next = left;
-		if (right < heap.size() && heap[next] > heap[right])
-			next = right;
-		if (next == here)
-			break;
-		swap(heap[here], heap[next]);
-		here = next;
-	}
-}
 
 void solve()
 {
+	priority_queue<int, vector<int>, greater<int>> pq;
+	int n;
 	cin >> n;
-	//heap.resize(n);
 
-	int temp;
-	for (int i = 0; i < n; ++i)
-	{
+	for (int i = 0; i < n; ++i) {
+		int temp;
 		cin >> temp;
-		//heap[i] = temp;
-		if (temp == 0)
-			pop_heap(heap);
-		else
-			push_heap(heap, temp);
-	}
-
-	for (int i = 0; i < ans.size(); ++i)
-	{
-		cout << ans[i] << '\n';
+		if (temp == 0) {
+			if (pq.empty() == false) {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+			else cout << 0 << '\n';
+		}
+		else {
+			pq.push(temp);
+		}
 	}
 }
 
 int main()
 {
+	FILE* stream;
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
 
 	solve();
+
+	return 0;
 }
