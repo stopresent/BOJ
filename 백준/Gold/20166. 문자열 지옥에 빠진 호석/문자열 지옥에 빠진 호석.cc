b14a1eb02155850803a7811a9ff38fd82d7ed866
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-11-06 11:26:29 +0900

    [Gold IV] Title: 문자열 지옥에 빠진 호석, Time: 20 ms, Memory: 22264 KB, Score: 12 point -BaekjoonHub

--- 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.cc
+++ 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.cc
@@ -3,61 +3,64 @@
 using namespace std;
 
 
-// 8방향 (상, 하, 좌, 우, 좌상, 우상, 좌하, 우하)
-int dx[8] = { -1, 1, 0, 0, -1, -1, 1, 1 };
 int dy[8] = { 0, 0, -1, 1, -1, 1, -1, 1 };
+int dx[8] = { -1, 1, 0, 0, -1, -1, 1, 1 };
 
 int N, M, K;
 vector<string> board;
-map<string, int> m;
+unordered_map<string, int> m;
+string s = "";
 
-void search(int x, int y) 
+void dfs(int y, int x)
 {
-	for (int i = 0; i < 8; i++) 
-    { // 각 방향에 대해
-		string cur = "";
-		int cx = x, cy = y;
-
-		for (int len = 0; len < K; len++) 
-        { // 길이 K까지
-			cur += board[cx][cy];
-			m[cur]++;
-
-			// 다음 위치로 이동
-			cx = (cx + dx[i] + N) % N;
-			cy = (cy + dy[i] + M) % M;
+	if (s.length() >= 5)
+	{
+		return;
+	}
+	else
+	{
+		for (int i = 0; i < 8; ++i)
+		{
+			int ny = (y + dy[i] + N) % N;
+			int nx = (x + dx[i] + M) % M;
+			s += board[ny][nx];
+			m[s]++;
+			dfs(ny, nx);
+			s.pop_back();
 		}
 	}
 }
 
 void solve()
 {
-    cin >> N >> M >> K;
-    board.resize(N);
+	cin >> N >> M >> K;
+	board.resize(N);
 
-    for (int i = 0; i < N; i++)
-    {
-        cin >> board[i];
-    }
+	for (int i = 0; i < N; i++)
+	{
+		cin >> board[i];
+	}
 
-    vector<string> v(K);
-    for (int i = 0; i < K; i++) 
-    {
-        cin >> v[i];
-    }
+	vector<string> v(K);
+	for (int i = 0; i < K; i++)
+	{
+		cin >> v[i];
+	}
 
-    for (int i = 0; i < N; i++) 
-    {
-        for (int j = 0; j < M; j++) 
-        {
-            search(i, j);
-        }
-    }
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			s.push_back(board[i][j]);
+			dfs(i, j);
+			s.clear();
+		}
+	}
 
-    for (const auto& a : v) 
-    {
-        cout << m[a] << '\n';
-    }
+	for (int i = 0; i < v.size(); ++i)
+	{
+		cout << m[v[i]] << '\n';
+	}
 }
 
 int main()
