58cf4dd453a03737f46683bf4cedfd8ddca078bf
Stopresent <86364202+stopresent@users.noreply.github.com>
2024-11-08 12:17:12 +0900

    [FIX] 테스트 원복

--- CSharp/Server/DummyClient/Program.cs
+++ CSharp/Server/DummyClient/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace DummyClient
+namespace DummyClient
 {
     internal class Program
     {
--- CSharp/Server/ServerCore/Program.cs
+++ CSharp/Server/ServerCore/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace ServerCore
+namespace ServerCore
 {
     internal class Program
     {
+        static bool _stop = false;
+
+        static void ThreadMain()
+        {
+            Console.WriteLine("쓰레드 시작!");
+
+            while (_stop == false)
+            {
+                // 누군가가 stop 신호를 해주기를 기다린다.
+            }
+
+            Console.WriteLine("쓰레드 종료!");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Task t = new Task(ThreadMain);
+            t.Start();
+
+            Thread.Sleep(1000);
+
+            _stop = true;
+
+            Console.WriteLine("stop 호출");
+            Console.WriteLine("종료 대기중");
+
+            t.Wait(); // task가 끝났는지 알 수 있다.
+
+            Console.WriteLine("종료 성공");
         }
     }
 }
