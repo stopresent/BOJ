fc8429139d8f1562a0e94a79808c32529afb30ce
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-11-16 08:25:46 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4180 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<int> numbers(n);
+    for (int i = 0; i < n; i++) {
+        cin >> numbers[i];
+    }
+
+    sort(numbers.begin(), numbers.end());
+
+    for (const int &num : numbers) {
+        cout << num << '\n';
+    }
+
+    return 0;
+}
