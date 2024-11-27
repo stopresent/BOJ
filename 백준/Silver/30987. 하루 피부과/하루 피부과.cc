8dd20367f919da9915f153f49da02cfec7541292
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-11-27 15:55:41 +0900

    [Silver V] Title: 하루 피부과, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/30987. 하루 피부과/하루 피부과.cc
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int x1, x2, a, b, c, d, e;
+	cin >> x1 >> x2 >> a >> b >> c >> d >> e;
+	b = b - d;
+	c = c - e;
+
+	a /= 3;
+	b /= 2;
+	
+	cout << (a * x2 * x2 * x2 + b * x2 * x2 + c * x2) - (a * x1 * x1 * x1 + b * x1 * x1 + c * x1);
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
