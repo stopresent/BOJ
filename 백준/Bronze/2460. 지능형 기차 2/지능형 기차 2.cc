3481b7a68dd8a354a9250b30dda6ed06dbbc759d
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2024-12-22 21:15:38 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, ret = 0, sum = 0;
+
+void solve()
+{
+	for (int i = 0; i < 10; ++i)
+	{
+		cin >> a >> b;
+		sum -= a;
+		sum += b;
+
+		ret = max(ret, sum);
+	}
+
+	cout << ret;
+}
+
+int main()
+{
+	FILE* stream;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
