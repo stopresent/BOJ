b2ef5078e9c1a94caf214f3db6912aeffcba39b2
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2025-04-17 10:54:09 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -3,98 +3,56 @@
 using namespace std;
 
 int N, M;
-set<int> s;
-vector<int> v;
-vector<int> indexes;
+vector<int> nums;
+vector<int> seq;
 
-void SetOrim(vector<int>& index)
+void dfs(int idx, int depth)
 {
-	int size = index.size();
-	for (int i = size - 1; i > 0; --i)
-	{
-		if (index[i] > index[i - 1])
-			index[i - 1] = index[i];
-	}
+    if (depth == M)
+    {
+        for (int i = 0; i < M; ++i)
+        {
+            cout << seq[i] << (i + 1 == M ? '\n' : ' ');
+        }
+        return;
+    }
+
+    int prev = -1;
+    for (int i = idx; i < N; ++i)
+    {
+        if (nums[i] == prev)
+        {
+            continue;
+        }
+        prev = nums[i];
+
+        seq.push_back(nums[i]);
+        dfs(i, depth + 1);
+        seq.pop_back();
+    }
 }
 
 void solve()
 {
-	cin >> N >> M;
-	int num;
+    cin >> N >> M;
+    nums.resize(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> nums[i];
+    }
+    sort(nums.begin(), nums.end());
 
-	for (int i = 0; i < N; ++i)
-	{
-		cin >> num;
-		s.insert(num);
-	}
-
-	for (auto i = s.begin(); i != s.end(); i++)
-	{
-		v.push_back(*i);
-	}
-
-	// indexes[0]이 1의 자리 수.
-	sort(v.begin(), v.end());
-	indexes.assign(M, 0);
-	int size = v.size();
-	bool flag = false;
-	while (indexes[M - 1] <= size - 1)
-	{
-		for (int i = 0; i < M; ++i)
-		{
-			if (i + 1 == M) continue;
-			if (indexes[M - 1] == size)
-			{
-				flag = true;
-				break;
-			}
-
-			for (int j = 0; j < M; ++j)
-			{
-				if (indexes[M - 1] == size)
-				{
-					flag = true;
-					break;
-				}
-
-				if (j + 1 == M)
-				{
-					break;
-				}
-
-				// 자리 수 올리기
-				if (indexes[j] >= size)
-				{
-					indexes[j] = 0;
-					indexes[j + 1]++;
-				}
-			}
-
-			SetOrim(indexes);
-
-		}
-
-		if (flag) break;
-
-		for (int i = M - 1; i >= 0; --i)
-		{
-			int index = indexes[i];
-			cout << v[index] << " ";
-		}
-		cout << '\n';
-		indexes[0]++;
-	}
+    dfs(0, 0);
 }
 
 int main()
 {
-	FILE* stream;
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	//freopen_s(&stream, "input.txt", "rt", stdin);
+    FILE* stream;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //freopen_s(&stream, "input.txt", "rt", stdin);
 
-	solve();
-
-	return 0;
+    solve();
+    return 0;
 }
