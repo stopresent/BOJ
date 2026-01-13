a602d9e8ba59f4d2afb2c3b8d7f89b1219d5cc2e
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2026-01-13 16:34:50 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,51 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <algorithm>
-#include <math.h>
-#include <climits>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+string str;
 
 void solve()
 {
-	string s;
-	getline(cin, s);
-
-	int gap = 13;
-	string ret = "";
-
-	for (int i = 0; i < s.size(); ++i)
+	getline(cin, str);
+	for (int i = 0; i < str.size(); i++)
 	{
-		if (s[i] >= 'A' && s[i] <= 'Z')
+		if (str[i] == 'u')
 		{
-			if (s[i] <= 'M')
-				s[i] += gap;
-			else
-				s[i] = 'A' + 13 - ('Z' - s[i]) - 1;
-			
-			ret += s[i];
+			int a = 3;
 		}
-		else if (s[i] >= 'a' && s[i] <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (s[i] <= 'm')
-				s[i] += gap;
+			if (str[i] > (('a' + 'z') / 2))
+				str[i] = str[i] + 13 - 26;
 			else
-				s[i] = 'a' + 13 - ('z' - s[i]) - 1;
-
-			ret += s[i];
+				str[i] = str[i] + 13;
 		}
-		else
-			ret += s[i];
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			if (str[i] > (('A' + 'Z') / 2))
+				str[i] = str[i] + 13 - 26;
+			else
+				str[i] = str[i] + 13;
+		}
 	}
 
-	cout << ret;
+	cout << str;
 }
 
 int main()
