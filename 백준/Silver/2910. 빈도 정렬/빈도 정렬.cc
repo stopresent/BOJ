7859c95b9b63bbaa23f751ab70454429850146e7
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2026-01-16 20:25:47 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,53 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include<iostream>
-#include <fstream>
-#include <vector>
-#include <algorithm>
-#include <math.h>
-#include <climits>
-#include <map>
-#include <unordered_map>
-#include <set>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, c, a[1004];
+int n, c, temp;
 vector<pair<int, int>> v;
-map<int, int> mp, mp_first;
+map<int, int> m, m_first; // m : 숫자, 개수 m_first : 숫자, 처음 나온 인덱스
+
 bool cmp(pair<int, int> a, pair<int, int> b)
 {
-	if (a.first == b.first)
-		return mp_first[a.second] < mp_first[b.second];
-	return a.first > b.first;
+	if (a.first == b.first) // 빈도가 같다면
+		return m_first[a.second] < m_first[b.second]; // 먼저 만난순
+	return a.first > b.first; // 빈도가 높은순으로 정렬
 }
 
 void solve()
 {
 	cin >> n >> c;
-	for (int i = 0; i < n; ++i)
+	for (int i = 0; i < n; i++)
 	{
-		cin >> a[i];
-		mp[a[i]]++;
-		if (mp_first[a[i]] == 0)
-			mp_first[a[i]] = i + 1;
+		cin >> temp;
+		m[temp]++;
+		if (m_first[temp] == 0)
+			m_first[temp] = i + 1;
 	}
 
-	for (auto it : mp)
-	{
-		v.push_back({ it.second, it.first });
-	}
+	for (auto it : m)
+		v.push_back({ it.second, it.first }); // 빈도, 숫자
 
 	sort(v.begin(), v.end(), cmp);
 	for (auto i : v)
 	{
-		for (int j = 0; j < i.first; ++j)
-		{
-			cout << i.second << " ";
-		}
+		for (int j = 0; j < i.first; j++)
+			cout << i.second << ' ';
 	}
 }
 
