8181cc1a4c193a67bde437f27ee3e4d93320d044
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2026-01-17 16:06:02 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -1,76 +1,54 @@
 #define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include<iostream>
-#include <fstream>
-#include <vector>
-#include <algorithm>
-#include <math.h>
-#include <climits>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int s1 = 0, s2 = 0;
-vector<int> v(3000), v2(3000);
+int n, score1, score2, num, total1, total2, curtime;
+string s;
 
-void solve()
+int time_to_sec(string s)
 {
-	cin >> n;
-	int t;
-	string time;
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> t;
-		cin >> time;
-		string ss1 = time.substr(0, 2);
-		string ss2 = time.substr(3, 2);
-		int temp = ((ss1[0] - '0') * 10 + (ss1[1] - '0')) * 60;
-		int temp2 = (ss2[0] - '0') * 10 + (ss2[1] - '0');
-		int st = temp + temp2;
-		for (int j = st; j < 48 * 60; ++j)
-		{
-			if (t == 1)
-				v[j]++;
-			else
-				v2[j]++;
-		}
-	}
-
-	int total1 = 0, total2 = 0;
-	for (int i = 0; i < 48 * 60; ++i)
-	{
-		if (v[i] > v2[i])
-			total1++;
-		else if (v[i] < v2[i])
-			total2++;
-	}
-
-	int t1m = total1 / 60, t1s = total1 % 60;
-	int t2m = total2 / 60, t2s = total2 % 60;
-
-	cout.width(2); // 폭을 5로 지정
-	cout.fill('0'); // 채움 문자는 '0'
-	cout << t1m << ":";
-	cout.width(2); // 폭을 5로 지정
-	cout.fill('0'); // 채움 문자는 '0'
-	cout << t1s << '\n';
-	cout.width(2); // 폭을 5로 지정
-	cout.fill('0'); // 채움 문자는 '0'
-	cout << t2m << ":";
-	cout.width(2); // 폭을 5로 지정
-	cout.fill('0'); // 채움 문자는 '0'
-	cout << t2s << '\n';
+	int ret = 0;
+	string a = s.substr(0, 2);
+	ret = ret + (a[0] - '0') * 10 * 60 + (a[1] - '0') * 60;
+	a = s.substr(3, 2);
+	ret = ret + (a[0] - '0') * 10 + (a[1] - '0');
+	return ret;
 }
 
-int main()
-{
+string sec_to_time(int sec) {
+	string ret = "";
+	int a = sec / 60;
+	int b = sec % 60;
+	string sub1 = to_string(a);
+	string sub2 = to_string(b);
+	if (sub1.size() == 1) sub1 = "0" + sub1;
+	if (sub2.size() == 1) sub2 = "0" + sub2;
+	ret = sub1 + ":" + sub2;
+	return ret;
+}
+
+void solve() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num >> s;
+		int time = time_to_sec(s);
+		if (score1 > score2) total1 += (time - curtime);
+		else if (score1 < score2) total2 += (time - curtime);
+		if (num == 1) score1++;
+		else score2++;
+		curtime = time;
+	}
+	int time = time_to_sec("48:00");
+	if (score1 > score2) total1 += (time - curtime);
+	else if (score1 < score2) total2 += (time - curtime);
+	cout << sec_to_time(total1) << '\n';
+	cout << sec_to_time(total2) << '\n';
+
+}
+
+int main() {
 	FILE* stream;
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	//freopen_s(&stream, "input.txt", "rt", stdin);
 
 	solve();
