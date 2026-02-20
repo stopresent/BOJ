a617c014d735f68057f2e8a6fa58cd9986094a36
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2026-02-20 20:24:11 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+int b;
+
+void solve() {
+	cin >> b;
+
+	cout << b * 10 / 11;
+}
+
+int main() {
+	FILE* stream;
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	//freopen_s(&stream, "input.txt", "rt", stdin);
+
+	solve();
+
+	return 0;
+}
