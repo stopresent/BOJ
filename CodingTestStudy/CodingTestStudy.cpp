ed894b2b800178a9ef5e448ed1abb77778afba33
HyeonJae Seo <mamy4938@naver.com>
2026-04-13 18:41:29 +0900

    u

--- CodingTestStudy/CodingTestStudy.cpp
+++ CodingTestStudy/CodingTestStudy.cpp
@@ -2,16 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-
-}
-
 int main() {
 	FILE* stream;
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	freopen_s(&stream, "input.txt", "rt", stdin);
-
-	solve();
 
 	return 0;
 }
