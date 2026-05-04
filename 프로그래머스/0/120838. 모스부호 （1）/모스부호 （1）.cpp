dcf12c2f9de0db7c0256a359d834759e960593ed
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2026-05-04 13:43:22 +0900

    [level 0] Title: 모스부호 (1), Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+string str[26] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+map<string, char> mp;
+
+string solution(string letter) {
+    string answer = "";
+    
+    // ".... . .-.. .-.. ---"
+    for (int i = 0; i < 26; i++)
+    {
+        mp[str[i]] = 'a' + i;
+    }
+    
+    string input = "";
+    for (int i = 0; i < letter.size(); i++)
+    {
+        if (letter[i] != ' ')
+            input += letter[i];
+        else
+        {
+            answer += mp[input];
+            input = "";
+        }
+    }
+    answer += mp[input];
+    return answer;
+}
