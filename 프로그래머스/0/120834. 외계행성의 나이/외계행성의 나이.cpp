c2ea5fa9fe75fbce9abc6506a1d542f0aa9d9672
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2026-05-04 11:37:31 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(int age) {
+    string answer = "";
+    while(age > 0)
+    {
+        answer += age % 10 + 'a';
+        age /= 10;
+    }
+    reverse(answer.begin(), answer.end());
+    return answer;
+}
