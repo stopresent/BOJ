0a11dd331ec830732bccd4deeb8c24c7bde31d33
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2026-05-05 17:32:46 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.cpp
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.cpp
@@ -3,23 +3,7 @@
 
 using namespace std;
 
-string Push(string str)
-{
-    string result = "";
-    
-    result += str[str.size() - 1];
-    string sub = str.substr(0, str.size() - 1);
-    result += sub;
-    return result;
-}
-
 int solution(string A, string B) {
-    int answer = 0;
-    
-    for (int i = 0; i < A.size(); i++)
-    {
-        if (A == B) return i;
-        A = Push(A);
-    }
-    return -1;
+    B += B;
+    return B.find(A);
 }
