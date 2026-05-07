dd0f05196db3416f55f976bec23898ffd02f76fa
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2026-05-07 11:28:53 +0900

    [level 0] Title: 안전지대, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int dy[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+int dx[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+
+int solution(vector<vector<int>> board) {
+    int answer = 0;
+    int n = board.size();
+    for (int y = 0; y < n; y++)
+    {
+        for (int x = 0; x < n; x++)
+        {
+            if (board[y][x] != 1) continue;
+            
+            for (int i = 0; i < 8; i++)
+            {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                
+                if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+                if (board[ny][nx] != 1) board[ny][nx] = 2;
+            }
+        }
+    }
+    
+    for (int y = 0; y < n; y++)
+    {
+        for (int x = 0; x < n; x++)
+        {
+            if (board[y][x] == 0) answer++;
+        }
+    }
+    return answer;
+}
