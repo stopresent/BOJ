d4b03cb188cc398a6183f989fd58e4b9193340ca
HyeonJae Seo <86364202+stopresent@users.noreply.github.com>
2026-05-08 16:29:08 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 259.11 ms, Memory: 239 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -11,17 +11,21 @@ vector<int> solution(vector<int> numbers) {
     
     for (int i = 1; i < numbers.size(); i++)
     {
+        // 스택이 비지않고, 스택의 탑 위치의 값보다 현재 수열 값이 클 때까지
         while(!st.empty() && numbers[st.top()] < numbers[i])
         {
+            // 정답 수열에 스택의 탑 위치에 오큰수 넣기
             answer[st.top()] = numbers[i];
             st.pop();
         }
-        st.push(i);
+        st.push(i); // 마지막은 어차피 -1이 들어감.
     }
     
+    // 스택에는 오큰수가 없는 위치값만 있음.
     while (!st.empty())
     {
-        answer[st.top()] = -1;
+        // 오큰수가 없으니까 -1로 세팅
+        answer[st.top()] = -1; 
         st.pop();
     }
     
